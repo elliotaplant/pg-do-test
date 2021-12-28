1bcfbddab38cb89c056d3bcd8fcecbc5f3c1b597
Elliot Plant <elliotplant@gmail.com>
2021-12-28 15:06:41 -0800

    Try connecting agin

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const { Client } = require('pg');
 
 console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
-// const client = new Client(process.env.DATABASE_URL);
-//
-// client.connect();
-//
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   client.end()
-// });
+const client = new Client(process.env.DATABASE_URL);
 
-console.log('hi', 19);
+client.connect();
 
-process.exit(0);
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+  process.exit(0);
+});
