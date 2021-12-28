6932a21e82464c63c7dc5e200be5a5a57b73dcd1
Elliot Plant <elliotplant@gmail.com>
2021-12-28 14:22:37 -0800

    Try running something

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+const client = new Client(process.env.DATABASE_URL);
+
+client.connect();
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+});
