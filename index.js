15e578e1ea4d77f468fa6c0a217ad59010b3ad8d
Elliot Plant <elliotplant@gmail.com>
2021-12-28 14:51:51 -0800

    Try not connecting to provision db

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { Client } = require('pg');
 
 console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
-const client = new Client(process.env.DATABASE_URL);
+// const client = new Client(process.env.DATABASE_URL);
+//
+// client.connect();
+//
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// });
 
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-});
+console.log('hi', 19);
