9d24f589f6fcab75be4c8f57987ad99257c495e0
Elliot Plant <elliotplant@gmail.com>
2021-12-28 15:21:11 -0800

    Try the env

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { Client } = require('pg');
 
 console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
-const client = new Client(process.env.DATABASE_URL);
+console.log('process.env.CA_CERT', process.env.CA_CERT);
+const client =
+// new Client(process.env.DATABASE_URL);
+// DB_USERNAME
+// DB_PASSWORD
+// DB_HOST
+// DB_PORT
+// DB_DATABASE
+// DB_SSLMODE
+new Client({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.CA_CERT,
+  },
+})
 
 client.connect();
 
